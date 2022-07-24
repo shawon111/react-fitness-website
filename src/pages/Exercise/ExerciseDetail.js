b3d34fd504e7e4897e552586c576a7125edfa7a4
shawon ahmmed <shawonahmmed08509@gmail.com>
2022-07-25 01:34:35 +0600

    detail component complete

--- src/pages/Exercise/ExerciseDetail.js
+++ src/pages/Exercise/ExerciseDetail.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/system';
+import {exerciseOptions, fetchData} from '../../utilities/fetchData';
+import Detail from '../../components/Pages/ExerciseDetail/Detail';
+import ExerciseVideos from '../../components/Pages/ExerciseDetail/ExerciseVideos';
+import SimilarExercises from '../../components/Pages/ExerciseDetail/SimilarExercises';
 
 const ExerciseDetail = () => {
+    const [exerciseDetails, setExerciseDetails] = useState({});
+    const {id} = useParams();
+
+    useEffect(()=>{
+        const fetchExerciseDetailData = async () => {
+            const exerciseDetailData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`, exerciseOptions);
+            setExerciseDetails(exerciseDetailData)
+        }
+        fetchExerciseDetailData();
+    }, [id])
+
     return (
-        <div>
-            
-        </div>
+        <Box pt="60px">
+            <Detail exerciseDetails={exerciseDetails} />
+            <ExerciseVideos />
+            <SimilarExercises />
+        </Box>
     );
 };
 
